bce45257db20923d7901cf238b84f799330dfde4
mayoreee <mayowaegbinola@gmail.com>
2022-06-19 13:39:58 +0100

    feat: update scripts

---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir ~/.chainlink-rinkeby
+mkdir ~/.chainlink-rinkeby/data
+mkdir ~/.chainlink-rinkeby/chainlink-volume
+echo "secret" > ~/.chainlink-rinkeby/chainlink-volume/password.txt
+echo "youremail@address.com\npassword" > ~/.chainlink-rinkeby/chainlink-volume/apicredentials.txt
