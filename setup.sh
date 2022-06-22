26d2f748a04a7f5ee710882b984aa5329f5abd6d
mayoreee <mayowaegbinola@gmail.com>
2022-06-22 16:39:45 +0100

    fix: update node operator auth data

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ mkdir ~/.chainlink-rinkeby
 mkdir ~/.chainlink-rinkeby/data
 mkdir ~/.chainlink-rinkeby/chainlink-volume
 echo "secret" > ~/.chainlink-rinkeby/chainlink-volume/password.txt
-echo "email@example.com\npassword" > ~/.chainlink-rinkeby/chainlink-volume/apicredentials.txt
+echo "email@example.com\n123456789" > ~/.chainlink-rinkeby/chainlink-volume/apicredentials.txt
